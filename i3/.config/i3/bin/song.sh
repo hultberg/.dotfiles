a9bfedf5602f66dcd5e554510a22e959a518f0a5
Edvin Hultberg <hultberg@mittnett.net>
2017-02-22 13:15:05 +0100

    Song information in i3status

---
+++ i3/.config/i3/bin/song.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Script to echo song information using playerctl
+# If spotify is not playing, nothing is echoed.
+#
+# @author Edvin Hultberg <post@hultberg.no>
+
+spotify_status=$(playerctl status)
+spotify_artist=$(playerctl metadata xesam:artist)
+spotify_song=$(playerctl metadata xesam:title)
+
+if [[ "$spotify_status" = "Playing" ]]; then
+	echo -n "#np $spotify_artist - $spotify_song" || exit 1
+else
+	echo -n ""
+fi
+
