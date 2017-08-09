19a4297e35a758be43757a6bea6ea8d01de3e04b
Edvin Hultberg <hultberg@mittnett.net>
2017-08-09 13:02:50 +0200

    script to setup vim

---
+++ vimsetup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd ~/.vim/colors && curl -LO https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
